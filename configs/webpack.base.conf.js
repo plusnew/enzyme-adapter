f71908bfec0d34caf0218aed104858f81ed833ae
plusgut <github@webentwickler2-0.de>
2018-03-16 20:09:37 +0100

    [webpack] added webpack to buildprocess

---
+++ configs/webpack.base.conf.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const { TsConfigPathsPlugin } = require('awesome-typescript-loader');
+
+module.exports = {
+  mode: 'development',
+  entry: ['./src/index.ts'],
+  output: {
+      path: __dirname + '/../dist',
+      filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    plugins: [
+      new TsConfigPathsPlugin(/* { configFileName, compiler } */)
+    ]
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      loader: 'awesome-typescript-loader',
+    }]
+  },
+};
---
+++ test/shallow.spec.tsx
@@ -0,0 +1,19 @@
+import enzymeAdapterPlusnew from 'index';
+import { configure, shallow } from 'enzyme';
+import plusnew, { component } from 'plusnew';
+
+configure({ adapter: new enzymeAdapterPlusnew() });
+
+describe('test App.tsx', () => {
+  it('true should be true', () => {
+    const App = component(
+      () => ({}),
+      () => <button />,
+    )
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(<button />).length).toBe(1);
+    expect(wrapper.find(<input />).length).toBe(0);
+
+  });
+});
+
