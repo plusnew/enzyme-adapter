0e84522f2558b8874c973b3b42e03346ca5b4180
Carlo Jeske <jeske@sc-networks.com>
2018-04-19 16:46:45 +0200

    [types events] added wrapper types and implemented eventsimulation

--- src/wrapper/PlusnewCommonWrapper.ts
+++ src/wrapper/PlusnewCommonWrapper.ts
@@ -8,160 +8,160 @@ type predicate = (instance: Instance) => boolean;
 abstract class PlusnewCommonWrapper {
   abstract wrapper: any;
 
-  public at(index: number) {
+  public at(index: number): this {
     return this.wrapper.at.apply(this.wrapper, arguments);
   }
 
-  public childAt() {
+  public childAt(): this {
     return this.wrapper.childAt.apply(this.wrapper, arguments);
   }
 
-  public children() {
+  public children(): this {
     return this.wrapper.children.apply(this.wrapper, arguments);
   }
 
-  public closest(selector: selector) {
+  public closest(selector: selector): this {
     return this.wrapper.closest.apply(this.wrapper, arguments);
   }
 
-  public contains(nodeOrNodes: plusnew.JSX.Element | plusnew.JSX.Element[]) {
+  public contains(nodeOrNodes: plusnew.JSX.Element | plusnew.JSX.Element[]): boolean {
     return this.wrapper.contains.apply(this.wrapper, arguments);
   }
 
-  public containsAllMatchingElements(nodes: plusnew.JSX.Element[]) {
+  public containsAllMatchingElements(nodes: plusnew.JSX.Element[]): boolean {
     return this.wrapper.containsAllMatchingElements.apply(this.wrapper, arguments);
   }
 
-  public containsAnyMatchingElements(nodes: plusnew.JSX.Element[]) {
+  public containsAnyMatchingElements(nodes: plusnew.JSX.Element[]): boolean {
     return this.wrapper.containsAnyMatchingElements.apply(this.wrapper, arguments);
   }
 
-  public containsMatchingElement(node: plusnew.JSX.Element) {
+  public containsMatchingElement(node: plusnew.JSX.Element): boolean {
     return this.wrapper.containsMatchingElement.apply(this.wrapper, arguments);
   }
 
-  public context(key: any) {
+  public context(key: any): never {
     throw new Error('Plusnew does not have contexts');
     // return this.wrapper.context.apply(this.wrapper, arguments);
   }
 
-  public debug() {
+  public debug(): string {
     return this.wrapper.debug.apply(this.wrapper, arguments);
   }
 
-  public every(selector: selector) {
+  public every(selector: selector): boolean {
     return this.wrapper.every.apply(this.wrapper, arguments);
   }
 
-  public everyWhere(predicate: predicate) {
+  public everyWhere(predicate: predicate): boolean {
     return this.wrapper.everyWhere.apply(this.wrapper, arguments);
   }
 
-  public exists() {
+  public exists(): boolean {
     return this.wrapper.exists.apply(this.wrapper, arguments);
   }
 
-  public filter(selector: selector) {
+  public filter(selector: selector): this {
     return this.wrapper.filter.apply(this.wrapper, arguments);
   }
 
-  public filterWhere(predicate: predicate) {
+  public filterWhere(predicate: predicate): this {
     return this.wrapper.filterWhere.apply(this.wrapper, arguments);
   }
 
-  public find(selector: selector) {
+  public find(selector: selector): this {
     return this.wrapper.find.apply(this.wrapper, arguments);
   }
 
-  public findWhere(predicate: predicate) {
+  public findWhere(predicate: predicate): this {
     return this.wrapper.findWhere.apply(this.wrapper, arguments);
   }
 
-  public first() {
+  public first(): this {
     return this.wrapper.first.apply(this.wrapper, arguments);
   }
 
-  public forEach(fn: any) {
+  public forEach(fn: any): this {
     return this.wrapper.forEach.apply(this.wrapper, arguments);
   }
 
-  public get(index: number) {
+  public get(index: number): plusnew.JSX.Element {
     return this.wrapper.get.apply(this.wrapper, arguments);
   }
 
-  public hasClass(className: string) {
+  public hasClass(className: string): boolean {
     return this.wrapper.hasClass.apply(this.wrapper, arguments);
   }
 
-  public html() {
+  public html(): string {
     return this.wrapper.html.apply(this.wrapper, arguments);
   }
 
-  public instance() {
+  public instance(): Instance {
     return this.wrapper.instance.apply(this.wrapper, arguments);
   }
 
-  public is(selector: selector) {
+  public is(selector: selector): boolean {
     return this.wrapper.is.apply(this.wrapper, arguments);
   }
 
-  public isEmpty() {
+  public isEmpty(): boolean {
     return this.wrapper.isEmpty.apply(this.wrapper, arguments);
   }
 
-  public key() {
+  public key(): number | string {
     return this.wrapper.key.apply(this.wrapper, arguments);
   }
 
-  public last() {
+  public last(): this {
     return this.wrapper.last.apply(this.wrapper, arguments);
   }
 
-  public map(fn: any) {
+  public map<T>(fn: (node: this) => T): T[] {
     return this.wrapper.map.apply(this.wrapper, arguments);
   }
 
-  public matchesElement(node: plusnew.JSX.Element) {
+  public matchesElement(node: plusnew.JSX.Element): boolean {
     return this.wrapper.matchesElement.apply(this.wrapper, arguments);
   }
 
-  public name() {
+  public name(): string | null {
     return this.wrapper.name.apply(this.wrapper, arguments);
   }
 
-  public not(selector: selector) {
+  public not(selector: selector): this {
     return this.wrapper.not.apply(this.wrapper, arguments);
   }
 
-  public parent() {
+  public parent(): this {
     return this.wrapper.parent.apply(this.wrapper, arguments);
   }
 
-  public parents() {
+  public parents(): this {
     return this.wrapper.parents.apply(this.wrapper, arguments);
   }
 
-  public prop(key: string) {
+  public prop(key: string): any {
     return this.wrapper.prop.apply(this.wrapper, arguments);
   }
 
-  public props() {
+  public props(): {[key: string]: any} {
     return this.wrapper.props.apply(this.wrapper, arguments);
   }
 
-  public reduce(fn: any, initialValue?: any) {
+  public reduce<T>(fn: (node: this) => T, initialValue?: T): T {
     return this.wrapper.reduce.apply(this.wrapper, arguments);
   }
 
-  public reduceRight(fn: any, initialValue?: any) {
+  public reduceRight<T>(fn: (node: this) => T, initialValue?: T): T {
     return this.wrapper.reduceRight.apply(this.wrapper, arguments);
   }
 
-  public render() {
+  public render(): any {
     return this.wrapper.render.apply(this.wrapper, arguments);
   }
 
-  public setContext(context: any) {
+  public setContext(context: any): never {
     throw new Error('Plusnew does not have contexts');
     // return this.wrapper.setContext.apply(this.wrapper, arguments);
   }
@@ -174,19 +174,19 @@ abstract class PlusnewCommonWrapper {
     return this.wrapper.setState.apply(this.wrapper, arguments);
   }
 
-  public simulate(event: string, data?: any) {
+  public simulate(event: string, data?: any): this {
     return this.wrapper.simulate.apply(this.wrapper, arguments);
   }
 
-  public slice(begin?: number, end?: number) {
+  public slice(begin?: number, end?: number): this {
     return this.wrapper.slice.apply(this.wrapper, arguments);
   }
 
-  public some(selector: selector) {
+  public some(selector: selector): boolean {
     return this.wrapper.some.apply(this.wrapper, arguments);
   }
 
-  public someWhere(predicate: predicate) {
+  public someWhere(predicate: predicate): boolean {
     return this.wrapper.someWhere.apply(this.wrapper, arguments);
   }
 
@@ -214,8 +214,7 @@ abstract class PlusnewCommonWrapper {
     return this.wrapper.update.apply(this.wrapper, arguments);
   }
 
-
-  // length: number;
+  length: number;
 }
 
 type Component = (props: any, instance: ComponentInstance) => plusnew.JSX.Element | null;
--- src/wrapper/PlusnewMountWrapper.ts
+++ src/wrapper/PlusnewMountWrapper.ts
@@ -8,15 +8,15 @@ export default class PlusnewMountWrapper extends PlusnewCommonWrapper {
     this.wrapper = wrapper;
   }
 
-  public detach() {
+  public detach(): void {
     return this.wrapper.detach.apply(this.wrapper, arguments);
   }
 
-  public mount() {
+  public mount(): this {
     return this.wrapper.mount.apply(this.wrapper, arguments);
   }
 
-  public ref(refName: string) {
+  public ref(refName: string): this {
     return this.wrapper.ref.apply(this.wrapper, arguments);
   }
 }
