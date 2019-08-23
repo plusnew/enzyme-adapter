36545114a980125d6222de33940e5e8290e64541
plusgut <github@webentwickler2-0.de>
2019-08-23 10:07:52 +0200

    [webpack] maade driver an external dependency

--- configs/webpack.prod.conf.js
+++ configs/webpack.prod.conf.js
@@ -3,6 +3,7 @@ const constants = require('./constants');
 const prodConfig = require('plusnew-webpack-config').prod('enzyme-adapter-plusnew', constants.baseDirectory);
 prodConfig.externals = {
   '@plusnew/core': '@plusnew/core',
+  '@plusnew/driver-dom': '@plusnew/driver-dom',
   enzyme: 'enzyme',
 };
 
