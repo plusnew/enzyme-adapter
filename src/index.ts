9b0a1d145f0a21813a23ff2e5a712f9cc8ed66e2
plusgut <github@webentwickler2-0.de>
2019-08-23 10:20:49 +0200

    [driver] made driver-dom loading required

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import EnzymeAdapterPlusnew from './EnzymeAdapterPlusnew';
 import shallow from './shallow';
 import mount from './mount';
 import { getAwaitAllHandle, getComponentPartial } from './helpers';
+import '@plusnew/driver-dom/dist/jsx';
+
 export default EnzymeAdapterPlusnew;
 
 export { shallow, mount, getAwaitAllHandle, getComponentPartial };
